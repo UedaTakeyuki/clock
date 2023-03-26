a737af32b991f72ab224f5c087719179f2f28427
Dr. Takeyuki Ueda <gde00107@nifty.com>
2023-03-26 11:32:03 +0900

    use fb0/virtual_size

--- qt5/display.cpp
+++ qt5/display.cpp
@@ -2,9 +2,27 @@
 #include <QMessageBox>
 #include <QProcess>
 #include <QDebug>
+#include <QFile>
 
 Display::Display()
 {
+    QFile file( "/sys/class/graphics/fb0/virtual_size" );
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "can not open file." ;
+        return;
+    }
+    QTextStream in(&file);
+    QString inStr = in.readAll();
+    file.close();
+    QStringList l = inStr.trimmed().split(",");
+
+    QString w = l[0];
+    QString h = l[1];
+
+
+/*
     // https://stackoverflow.com/a/17344946/11073131
     QProcess pingProcess;
     QString exec = "tvservice";
@@ -19,9 +37,10 @@ Display::Display()
     r.exactMatch(reso);
     QStringList l = r.capturedTexts();
 
+
     QString w = l[1];
     QString h = l[2];
-
+*/
 /*
     QMessageBox Msgbox;
     Msgbox.setText(w);
